f629b131d44409d54f2130d8e11fd2c5582854ec
kl3eo <alex@motivation.ru>
2022-08-18 15:11:12 +0300

    sync club/uni code and some fix

--- vchat/src/main/resources/static/js/i18n_120.js
+++ vchat/src/main/resources/static/js/i18n_120.js
@@ -25,22 +25,13 @@ var help = new Map([
    ['pt', 'ajuda'],
 ]);
 
-var phones = new Map([
-   ['en', '..more ROOMS! <a href="https://room-house.com" target=new>here</a>'],
-   ['ru', '..больше КОМНАТ! <a href="https://room-house.com" target=new>тут</a>'],
-   ['es', '..más HABITACIONES! <a href="https://room-house.com" target=new>aquí</a>'],
-   ['fr', '..plus de CHAMBRES! <a href="https://room-house.com" target=new>ici</a>'],
-   ['cn', '..更多房间! <a href="https://room-house.com" target=new>這裡</a>'],
-   ['pt', '..mais QUARTOS! <a href="https://room-house.com" target=new>aqui</a>'],
-]);
-
 var warner = new Map([
-   ['en', 'RE-CONNECTING..'],
-   ['ru', 'соединение устанавливается..'],
-   ['es', 'RECONECTANDO..'],
-   ['fr', 'RECONNEXION..'],
-   ['cn', 'RE-CONNECTING..'],
-   ['pt', 'RECONECTANDO..'],
+   ['en', 'RE-CONNECTING ..'],
+   ['ru', 'соединение ..'],
+   ['es', 'RECONECTANDO ..'],
+   ['fr', 'RECONNEXION ..'],
+   ['cn', 'RE-CONNECTING ..'],
+   ['pt', 'RECONECTANDO ..'],
 ]);
 
 var badger_ = new Map([
@@ -124,14 +115,6 @@ var anno_adder_content = new Map([
    ['pt', 'SET ANNOTATION: <input type=text id=asender value="" onkeydown="var Ucode=event.keyCode? event.keyCode : event.charCode; if (Ucode == 13) {if (this.value.length) {sendMessage({id : \'setAnno\', anno:this.value.replace(/\'/g, \'\')});mod7.close();} else {this.placeholder = \'???\';}}">'],
 ]);
 
-var new_account = new Map([
-   ['en', 'Your account ID (copy/paste and hit Enter) <input type=text id=rsender style="width:210px;" value="" onkeydown="var Ucode=event.keyCode? event.keyCode : event.charCode; if (Ucode == 13) {if (this.value.length == 48 && this.value.substring(0,1) == \'5\') {setCookie(\'acc\',this.value,144000);mod6.close();$(\'sp_balance\').src=\'https://cube.room-house.com:8444/?acc=\'+this.value;leaveRoom();register();} else {this.value=\'\';this.placeholder = \'Please enter valid address\';}}"><br>Create <a href=\'https://wallet.room-house.com/#/accounts/0\' target=\'new\'>a new account</a>.'],
-      ['es', 'Your account ID (copy/paste and hit Enter) <input type=text id=rsender style="width:210px;" value="" onkeydown="var Ucode=event.keyCode? event.keyCode : event.charCode; if (Ucode == 13) {if (this.value.length == 48 && this.value.substring(0,1) == \'5\') {setCookie(\'acc\',this.value,144000);mod6.close();$(\'sp_balance\').src=\'https://cube.room-house.com:8444/?acc=\'+this.value;leaveRoom();register();} else {this.value=\'\';this.placeholder = \'Please enter valid address\';}}"><br>Create <a href=\'https://wallet.room-house.com/#/accounts/0\' target=\'new\'>a new account</a>.'],
-         ['pt', 'Your account ID (copy/paste and hit Enter) <input type=text id=rsender style="width:210px;" value="" onkeydown="var Ucode=event.keyCode? event.keyCode : event.charCode; if (Ucode == 13) {if (this.value.length == 48 && this.value.substring(0,1) == \'5\') {setCookie(\'acc\',this.value,144000);mod6.close();$(\'sp_balance\').src=\'https://cube.room-house.com:8444/?acc=\'+this.value;leaveRoom();register();} else {this.value=\'\';this.placeholder = \'Please enter valid address\';}}"><br>Create <a href=\'https://wallet.room-house.com/#/accounts/0\' target=\'new\'>a new account</a>.'],
-	    ['fr', 'Your account ID (copy/paste and hit Enter) <input type=text id=rsender style="width:210px;" value="" onkeydown="var Ucode=event.keyCode? event.keyCode : event.charCode; if (Ucode == 13) {if (this.value.length == 48 && this.value.substring(0,1) == \'5\') {setCookie(\'acc\',this.value,144000);mod6.close();$(\'sp_balance\').src=\'https://cube.room-house.com:8444/?acc=\'+this.value;leaveRoom();register();} else {this.value=\'\';this.placeholder = \'Please enter valid address\';}}"><br>Create <a href=\'https://wallet.room-house.com/#/accounts/0\' target=\'new\'>a new account</a>.'],
-	       ['cn', 'Your account ID (copy/paste and hit Enter) <input type=text id=rsender style="width:210px;" value="" onkeydown="var Ucode=event.keyCode? event.keyCode : event.charCode; if (Ucode == 13) {if (this.value.length == 48 && this.value.substring(0,1) == \'5\') {setCookie(\'acc\',this.value,144000);mod6.close();$(\'sp_balance\').src=\'https://cube.room-house.com:8444/?acc=\'+this.value;leaveRoom();register();} else {this.value=\'\';this.placeholder = \'Please enter valid address\';}}"><br>Create <a href=\'https://wallet.room-house.com/#/accounts/0\' target=\'new\'>a new account</a>.'],
-	          ['ru', 'Your account ID (copy/paste and hit Enter) <input type=text id=rsender style="width:210px;" value="" onkeydown="var Ucode=event.keyCode? event.keyCode : event.charCode; if (Ucode == 13) {if (this.value.length == 48 && this.value.substring(0,1) == \'5\') {setCookie(\'acc\',this.value,144000);mod6.close();$(\'sp_balance\').src=\'https://cube.room-house.com:8444/?acc=\'+this.value;leaveRoom();register();} else {this.value=\'\';this.placeholder = \'Please enter valid address\';}}"><br>Create <a href=\'https://wallet.room-house.com/#/accounts/0\' target=\'new\'>a new account</a>.']
-]);
 
 var left_label = new Map([
    ['en', 'for MESSI:'],
@@ -151,15 +134,6 @@ var right_label = new Map([
    ['pt', 'por RONALDO:'],
 ]);
 
-var for_por = new Map([
-   ['en', 'for'],
-   ['ru','за'],   
-   ['es', 'por'],
-   ['fr', 'pour'],
-   ['cn', 'for'],
-   ['pt', 'por'],
-]);
-
 var sorry_ = new Map([
    ['en', 'Sorry'],
    ['ru', 'Извините, никого нет.'],
@@ -167,15 +141,6 @@ var sorry_ = new Map([
    ['fr', 'Désolé'],
    ['cn', 'Sorry'],
    ['pt', 'Desculpe'],
-]);
-
-var hola_ = new Map([
-   ['en', 'Hello, guys!'],
-   ['ru','Привет, ребята!'],   
-   ['es', '¡Hola chicos!'],
-   ['fr', 'Bonjour gars!'],
-   ['cn', 'Hello, guys!'],
-   ['pt', 'Ola pessoal!'],
 ]);
 
 var waiter_ = new Map([
--- vchat/src/main/resources/static/js/unused.js
+++ vchat/src/main/resources/static/js/unused.js
@@ -39,3 +39,41 @@
     }
 
   }
+
+// i18n.js
+
+var phones = new Map([
+   ['en', '..more ROOMS! <a href="https://room-house.com" target=new>here</a>'],
+   ['ru', '..больше КОМНАТ! <a href="https://room-house.com" target=new>тут</a>'],
+   ['es', '..más HABITACIONES! <a href="https://room-house.com" target=new>aquí</a>'],
+   ['fr', '..plus de CHAMBRES! <a href="https://room-house.com" target=new>ici</a>'],
+   ['cn', '..更多房间! <a href="https://room-house.com" target=new>這裡</a>'],
+   ['pt', '..mais QUARTOS! <a href="https://room-house.com" target=new>aqui</a>'],
+]);
+
+var for_por = new Map([
+   ['en', 'for'],
+   ['ru','за'],   
+   ['es', 'por'],
+   ['fr', 'pour'],
+   ['cn', 'for'],
+   ['pt', 'por'],
+]);
+
+var hola_ = new Map([
+   ['en', 'Hello, guys!'],
+   ['ru','Привет, ребята!'],   
+   ['es', '¡Hola chicos!'],
+   ['fr', 'Bonjour gars!'],
+   ['cn', 'Hello, guys!'],
+   ['pt', 'Ola pessoal!'],
+]);
+
+var new_account = new Map([
+   ['en', 'Your account ID (copy/paste and hit Enter) <input type=text id=rsender style="width:210px;" value="" onkeydown="var Ucode=event.keyCode? event.keyCode : event.charCode; if (Ucode == 13) {if (this.value.length == 48 && this.value.substring(0,1) == \'5\') {setCookie(\'acc\',this.value,144000);mod6.close();$(\'sp_balance\').src=\'https://cube.room-house.com:8444/?acc=\'+this.value;leaveRoom();register();} else {this.value=\'\';this.placeholder = \'Please enter valid address\';}}"><br>Create <a href=\'https://wallet.room-house.com/#/accounts/0\' target=\'new\'>a new account</a>.'],
+      ['es', 'Your account ID (copy/paste and hit Enter) <input type=text id=rsender style="width:210px;" value="" onkeydown="var Ucode=event.keyCode? event.keyCode : event.charCode; if (Ucode == 13) {if (this.value.length == 48 && this.value.substring(0,1) == \'5\') {setCookie(\'acc\',this.value,144000);mod6.close();$(\'sp_balance\').src=\'https://cube.room-house.com:8444/?acc=\'+this.value;leaveRoom();register();} else {this.value=\'\';this.placeholder = \'Please enter valid address\';}}"><br>Create <a href=\'https://wallet.room-house.com/#/accounts/0\' target=\'new\'>a new account</a>.'],
+         ['pt', 'Your account ID (copy/paste and hit Enter) <input type=text id=rsender style="width:210px;" value="" onkeydown="var Ucode=event.keyCode? event.keyCode : event.charCode; if (Ucode == 13) {if (this.value.length == 48 && this.value.substring(0,1) == \'5\') {setCookie(\'acc\',this.value,144000);mod6.close();$(\'sp_balance\').src=\'https://cube.room-house.com:8444/?acc=\'+this.value;leaveRoom();register();} else {this.value=\'\';this.placeholder = \'Please enter valid address\';}}"><br>Create <a href=\'https://wallet.room-house.com/#/accounts/0\' target=\'new\'>a new account</a>.'],
+	    ['fr', 'Your account ID (copy/paste and hit Enter) <input type=text id=rsender style="width:210px;" value="" onkeydown="var Ucode=event.keyCode? event.keyCode : event.charCode; if (Ucode == 13) {if (this.value.length == 48 && this.value.substring(0,1) == \'5\') {setCookie(\'acc\',this.value,144000);mod6.close();$(\'sp_balance\').src=\'https://cube.room-house.com:8444/?acc=\'+this.value;leaveRoom();register();} else {this.value=\'\';this.placeholder = \'Please enter valid address\';}}"><br>Create <a href=\'https://wallet.room-house.com/#/accounts/0\' target=\'new\'>a new account</a>.'],
+	       ['cn', 'Your account ID (copy/paste and hit Enter) <input type=text id=rsender style="width:210px;" value="" onkeydown="var Ucode=event.keyCode? event.keyCode : event.charCode; if (Ucode == 13) {if (this.value.length == 48 && this.value.substring(0,1) == \'5\') {setCookie(\'acc\',this.value,144000);mod6.close();$(\'sp_balance\').src=\'https://cube.room-house.com:8444/?acc=\'+this.value;leaveRoom();register();} else {this.value=\'\';this.placeholder = \'Please enter valid address\';}}"><br>Create <a href=\'https://wallet.room-house.com/#/accounts/0\' target=\'new\'>a new account</a>.'],
+	          ['ru', 'Your account ID (copy/paste and hit Enter) <input type=text id=rsender style="width:210px;" value="" onkeydown="var Ucode=event.keyCode? event.keyCode : event.charCode; if (Ucode == 13) {if (this.value.length == 48 && this.value.substring(0,1) == \'5\') {setCookie(\'acc\',this.value,144000);mod6.close();$(\'sp_balance\').src=\'https://cube.room-house.com:8444/?acc=\'+this.value;leaveRoom();register();} else {this.value=\'\';this.placeholder = \'Please enter valid address\';}}"><br>Create <a href=\'https://wallet.room-house.com/#/accounts/0\' target=\'new\'>a new account</a>.']
+]);
