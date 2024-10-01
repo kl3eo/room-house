a1629ce8a5eda305dd95a2574b74ff7052e59330
kl3eo <alex@motivation.ru>
2024-10-01 22:14:24 +0300

    new RH bootloader for v1.41

--- rh/create_rh.sh
+++ rh/create_rh.sh
@@ -11,13 +11,13 @@ fi
 
 vboxmanage createvm --name RH --ostype RedHat_64 --register --basefolder `pwd`
 mv loop_rh.vdi RH/ && cd RH
-vboxmanage modifyvm RH --memory 5120 --cpus 2 --audio none --firmware efi --nic1 bridged --nictype1 virtio --bridgeadapter1 enp0s31f6
+vboxmanage modifyvm RH --memory 4096 --cpus 2 --audio none --firmware efi --nic1 bridged --nictype1 virtio --bridgeadapter1 enp0s31f6
 #vboxmanage modifyvm RH --memory 4096 --cpus 2 --audio none --firmware efi --nic1 nat --nictype1 Am79C973 --nataliasmode1 proxyonly
 #vboxmanage modifyvm RH --natpf1 "chat,tcp,,8443,,443"
 #vboxmanage modifyvm RH --natpf1 "admin,tcp,,8843,,8443"
 #vboxmanage modifyvm RH --natpf1 "ssh,tcp,,2222,,22"
-vboxmanage createmedium --filename 2G.vdi --size 2048
+vboxmanage createmedium --filename 3G.vdi --size 3072
 vboxmanage storagectl RH --name SATA --add sata
 vboxmanage storageattach RH --storagectl SATA --medium loop_rh.vdi --port 0 --type hdd
-vboxmanage storageattach RH --storagectl SATA --medium 2G.vdi --port 1 --type hdd
+vboxmanage storageattach RH --storagectl SATA --medium 3G.vdi --port 1 --type hdd
 vboxmanage modifyvm RH --boot1 disk --boot2 none --boot3 none --boot4 none
