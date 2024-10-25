d5d74df9326ee9ca13c3e43161bd6161a92d0c6d
kl3eo <alex@motivation.ru>
2024-10-25 06:53:59 +0300

    test RH in Ubu 24.10

---
+++ rh/create_rh_uti.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p ~/VB && cd ~/VB
+
+if [ -f /opt/loop_rh_uti.vdi ]; then
+        cp -a /opt/loop_rh_uti.vdi ./
+else
+        echo File /opt/loop_rh_uti.vdi not found. Exiting
+        exit
+fi
+
+vboxmanage createvm --name RHU --ostype RedHat_64 --register --basefolder `pwd`
+mv loop_rh_uti.vdi RHU/ && cd RHU
+vboxmanage modifyvm RHU --memory 5120 --cpus 2 --audio none --firmware efi --nic1 bridged --nictype1 virtio --bridgeadapter1 enp0s31f6
+vboxmanage createmedium --filename 3G.vdi --size 3072
+vboxmanage storagectl RHU --name SATA --add sata
+vboxmanage storageattach RHU --storagectl SATA --medium loop_rh_uti.vdi --port 0 --type hdd
+vboxmanage storageattach RHU --storagectl SATA --medium 3G.vdi --port 1 --type hdd
+vboxmanage modifyvm RHU --boot1 disk --boot2 none --boot3 none --boot4 none
